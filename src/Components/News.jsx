931e5d96bff8681cc7f667cd83940079bdd692cc
gaurav-jain000 <116086566+gaurav-jain000@users.noreply.github.com>
2022-11-18 23:19:03 +0530

    msg

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Loader1 from './Loader1'
 
 const News = () => {
-  // News API key = 9cec3c7e4af441cfb361513c09c67a09
   const [articles, setArticles] = useState(null)
+  const [check, setCheck] = useState(0)
   const generateNews = () => {
+    if(check === 0){
+      setCheck(check + 1)
+    }
     try {
-      fetch("https://newsapi.org/v2/everything?q=Quiz&apiKey=9cec3c7e4af441cfb361513c09c67a09")
+      fetch("https://newsdata.io/api/1/news?apikey=pub_136410ad4de3102e1ae0a612b7642a735092f&language=en&page=30")
         .then(res => res.json())
         .then(data => {
-          setArticles(data.articles)
+          setArticles(data.results)
         })
     } catch (err) {
       console.log(err)
     }
   }
-  useEffect(() => {
-    generateNews();
-  }, [])
 
   return (
     <>
       <div className='news-section-head h-0 rounded-top bg-dark text-light py-2 px-3'>
-        Recent News of our Quiz world
+        Recent News
       </div>
-      <div className='news-section-body rounded-bottom bg-grey py-2 px-2'>
+      <div className={`news-section-body ${check ? "vh-75" : "vh-75"} d-flex flex-column align-items-center rounded-bottom bg-grey`}>
+        <button className='btn btn-primary rounded-pill my-2 px-3' onClick={generateNews}> Fetch News </button>
         {
           articles
             ?
@@ -33,10 +34,13 @@ const News = () => {
                 {
                   articles.map((elem, index) => {
                     return (
-                      <li key={index} className="w-100 border-bottom p-3 text-center d-inline-block">
-                        <img className="pb-3" src={elem.urlToImage} alt="" />
-                        <br />
-                        <a href={elem.url}>{elem.title}</a>
+                      <li key={index} className="w-100 border-bottom p-3 text-justify d-inline-block">
+                        <a href={elem.link}>{elem.title}</a> <br />
+                        <p>
+                          <span className="fs-6">{elem.description}</span>
+                          <br />
+                          <span className='text-secondary'> - by {elem.source_id}</span>
+                        </p>
                       </li>
                     )
                   })
@@ -44,9 +48,11 @@ const News = () => {
               </ul>
             </div>
             :
-            <Loader1 text="Loading News" loaderColor="dark" />
+            <div className={`${check ? "" : "display-hidden"}`}>
+              <Loader1 text="Loading News" loaderColor="dark" />
+            </div>
         }
-        <div className='scroll-div sticky-bottom text-warning text-center p-3 h5'>
+        <div className={`scroll-div w-100 ${articles ? "sticky-bottom" : "display-hidden"} text-light text-center m-0 py-3`}>
           Scroll for more...
         </div>
       </div>
