e2aa6730154830994ab1577cb1388faeb7d56768
gaurav-jain000 <116086566+gaurav-jain000@users.noreply.github.com>
2022-12-04 16:35:09 +0530

    setting site to be live again, i am an idiot

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/clanquizsite'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
